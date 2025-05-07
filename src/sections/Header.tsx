a3cfd6e9b9f95525fc889f524753c5148a895906
adrisad <22082002andrademedina@email.com>
2025-05-06 22:35:11 -0400

    style: proceso inicial del Header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,20 @@
+import LogoIcon from '@/assets/logo.svg'
 export const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div className=" flex justify-center items-center h-auto">
+        <div className="flex justify-between items-center">
+          <div className="flex justify-center items-center border border-white/15 w-10 h-10 rounded-lg">
+            <LogoIcon className=" w-8 h-8" />
+          </div>
+          <div className="flex justify-center items-center">
+            <button>Join waitlist</button>
+            <div>
+              menu
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
